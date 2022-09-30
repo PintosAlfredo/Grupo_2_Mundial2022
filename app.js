be0add871990ad784326b3de451c1481ddc73717
gabriel_marasco <gamarasco4@gmail.com>
2022-09-30 12:14:59 -0300

    modifique app.js e index.html

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const publicPath = path.resolve(__dirname, './public')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/index.html"))
+})
+
+app.use(express.static('public'))
+
+app.listen(8000, ()=>console.log('servidor corriendo'))
+ 
