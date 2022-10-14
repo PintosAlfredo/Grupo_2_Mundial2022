2522791d3a667f6d4dd824ddaaebc92d2cc21b19
min <chmink@gmail.com>
2022-10-14 11:44:25 -0300

    subiendo actualizaciones de estructura

--- app.js
+++ app.js
@@ -3,32 +3,31 @@ const path = require('path')
 const app = express();
 const publicPath = path.resolve(__dirname, './public')
 const port = process.env.PORT || 8000
-
+app.use(express.static(publicPath))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"./views/index.html"))
 })
 
-app.get('/productos', (req, res) => {
-    res.sendFile(path.join(__dirname,"./views/productos.html"))
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/product.html"))
 })
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname,"./views/login.html"))
+app.get('/log', (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/log.html"))
 })
-app.get('/signin', (req, res) => {
-    res.sendFile(path.join(__dirname,"./views/signin.html"))
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/register.html"))
 })
 app.get('/shop', (req, res) => {
     res.sendFile(path.join(__dirname,"./views/shop.html"))
 })
-app.get('/aboutus', (req, res) => {
-    res.sendFile(path.join(__dirname,"./views/nosotros.html"))
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/about.html"))
 })
-app.get('/contacto', (req, res) => {
-    res.sendFile(path.join(__dirname,"./views/contacto.html"))
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/contact.html"))
 })
-app.use(express.static('public'))
 
-app.listen(port, ()=>console.log(`servidor corriendo en puerto ${port}`))
+app.listen(port, ()=>console.log(`Servidor corriendo en puerto ${port}`))
  
  
